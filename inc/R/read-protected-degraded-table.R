92fd3ca3d4b08c08040f35249308104ddec35b60
jrfep <jr.ferrer.paris@gmail.com>
2020-09-25 11:01:26 +1000

    update tables with WDPA data to the present

--- inc/R/read-protected-degraded-table.R
+++ inc/R/read-protected-degraded-table.R
@@ -18,29 +18,6 @@ for (arch in dir(sprintf("%s/indicative-maps/version-2.0.1b/",work.dir),"xml",fu
   EFG.names <- c(EFG.names,xml_text(xml_find_all(x,"//Short-name")))
 }
 
-## output of inc/gras/rcross/combined-indicators.sh
-versions <- dir(sprintf("%s/output",work.dir))
-
-maps.x.indicators <- data.frame()
-for (ver in versions) {
-  archs <- dir(sprintf("%s/output/%s",work.dir,ver),pattern="Protected_Degraded_2013")
-  for (arch in archs) {
-     dts <- read.table(sprintf("%s/output/%s/%s",work.dir,ver,arch),col.names=c("WDPA","HFP","MCHI","map","area_m2"),stringsAsFactors=FALSE)
-     dts <- subset(dts,!map %in% "*")
-     dts$map <- 1+(as.numeric(dts$map)>1.5)
-
-     dts$map_code <- gsub("Protected_Degraded_2013_|.txt","",basename(arch))
-     dts$EFG <- strsplit(dts$map_code,".IM.")[[1]][1]
-     dts$version <- ver
-     dts$area <- dts$area_m2/1e6
-     maps.x.indicators <- rbind(maps.x.indicators,dts)
-  }
-}
-
-
-save(file=sprintf("%s/Rdata/Degraded-protected-2013-all-versions.rda", script.dir), maps.x.indicators, EFG.names)
-
-
 #choosing colors
 
 clrs <- ##brewer.pal(8,"Accent")
@@ -54,40 +31,67 @@ clr2 <- inferno(6)
 
  d.legend <- data.frame(lab=biome.labels, pch=c(17,17,17,15,15,15,15,15,16,16,16,16,16,16,17,0), col=clr2[c(1,6,3,4,2,1,5,6,1:6,2,4)], stringsAsFactors=F)
 
+## output of inc/gras/rcross/combined-indicators.sh
+versions <- dir(sprintf("%s/output",work.dir))
+for (k in c("2013","all")) {
+  maps.x.indicators <- data.frame()
+  for (ver in versions) {
+    archs <- dir(sprintf("%s/output/%s",work.dir,ver),pattern=sprintf("Protected_Degraded_%s",k))
+    for (arch in archs) {
+       dts <- read.table(sprintf("%s/output/%s/%s",work.dir,ver,arch),col.names=c("WDPA","HFP","MCHI","map","area_m2"),stringsAsFactors=FALSE)
+       dts <- subset(dts,!map %in% "*")
+       dts$map <- 1+(as.numeric(dts$map)>1.5)
 
- ## review EFG data
-
- slc <- unique(maps.x.indicators$EFG)
- ## exclude the anthropogenic
- slc <- slc[!(grepl("^F3.?",slc) | grepl("^T7.?",slc) | grepl("^M4.?",slc) | grepl("^MT3.?",slc) | grepl("^S2.?",slc) | grepl("^SF2.?",slc))]
- ## Ice and snow groups in the southern hemisphere are not well covered by human impact variables
- # slc <- slc[!slc %in% c("T6.1","T6.2","M2.5","F2.10")]
- ## Subterranean EFG are not well covered by the protection/degradation variables
- slc <- slc[!grepl("^S",slc)]
-
-
- EFG.dts <- maps.x.indicators %>% filter(EFG %in% slc)
-
- EFG.dts$clase <- "unknown"
-
- EFG.dts$clase[with(EFG.dts,(HFP %in% 1) | (MCHI %in% 1))] <- "degraded"
- EFG.dts$clase[with(EFG.dts,(WDPA %in% 1))] <- "protected"
- EFG.dts$clase[with(EFG.dts,((HFP %in% 0) | (MCHI %in% 0)) & (WDPA %in% "*"))] <- "wild unprotected"
-
- EFG.dts$realm <- gsub("[0-9.]","",EFG.dts$EFG)
- EFG.dts$biome <- gsub("\\.[0-9]+","",EFG.dts$EFG)
-
- EFG.dts$biome.lab <- biome.labels[pmatch(EFG.dts$biome,biome.labels,duplicates.ok=T)]
-
-## separate pixels from terrestrial and marine areas
- EFG.dts$terrestrial <- EFG.dts$HFP %in% c("0","1")
- EFG.dts$marine <- EFG.dts$MCHI %in% c("0","1")
-
-## This will allow to filter out areas with inconsistent data (terrestrial ecosystems in marine areas and viceversa)
+       dts$map_code <- gsub(sprintf("Protected_Degraded_%s_|.txt",k),"",basename(arch))
+       dts$EFG <- strsplit(dts$map_code,".IM.")[[1]][1]
+       dts$version <- ver
+       dts$area <- dts$area_m2/1e6
+       maps.x.indicators <- rbind(maps.x.indicators,dts)
+    }
+  }
+  ## review EFG data
+  slc <- unique(maps.x.indicators$EFG)
+  ## exclude the anthropogenic
+  slc <- slc[!(grepl("^F3.?",slc) | grepl("^T7.?",slc) | grepl("^M4.?",slc) | grepl("^MT3.?",slc) | grepl("^S2.?",slc) | grepl("^SF2.?",slc))]
+  ## Ice and snow groups in the southern hemisphere are not well covered by human impact variables
+  # slc <- slc[!slc %in% c("T6.1","T6.2","M2.5","F2.10")]
+  ## Subterranean EFG are not well covered by the protection/degradation variables
+  slc <- slc[!grepl("^S",slc)]
 
 
-##  large lakes have several water (no data) pixels in terrestrial areas
-EFG.dts %>% pivot_wider(
+  EFG.dts <- maps.x.indicators %>% filter(EFG %in% slc)
+
+  EFG.dts$clase <- "unknown"
+
+  EFG.dts$clase[with(EFG.dts,(HFP %in% 1) | (MCHI %in% 1))] <- "degraded"
+  EFG.dts$clase[with(EFG.dts,(WDPA %in% 1))] <- "protected"
+  EFG.dts$clase[with(EFG.dts,((HFP %in% 0) | (MCHI %in% 0)) & (WDPA %in% "*"))] <- "wild unprotected"
+
+  EFG.dts$realm <- gsub("[0-9.]","",EFG.dts$EFG)
+  EFG.dts$biome <- gsub("\\.[0-9]+","",EFG.dts$EFG)
+
+  EFG.dts$biome.lab <- biome.labels[pmatch(EFG.dts$biome,biome.labels,duplicates.ok=T)]
+
+ ## separate pixels from terrestrial and marine areas
+  EFG.dts$terrestrial <- EFG.dts$HFP %in% c("0","1")
+  EFG.dts$marine <- EFG.dts$MCHI %in% c("0","1")
+
+ ## This will allow to filter out areas with inconsistent data (terrestrial ecosystems in marine areas and viceversa)
+
+
+ ##  large lakes have several water (no data) pixels in terrestrial areas
+ EFG.dts %>% pivot_wider(
+      id_cols = c("EFG","version","realm","biome","biome.lab","terrestrial","marine"),
+      names_from = clase,
+      values_from = area,
+      values_fn = list(area=sum)
+      ) %>%
+      replace_na(list(degraded=0,protected=0,unknown=0,`wild unprotected`=0)) %>%
+        mutate(total=degraded+unknown+protected+`wild unprotected`) %>%
+          mutate(degraded=degraded*100/total,protected=protected*100/total,`wild unprotected`=`wild unprotected`*100/total,unknown=unknown*100/total) %>% filter(EFG %in% "F2.1") %>% select(marine,terrestrial,version,protected,degraded,`wild unprotected`,unknown,total)
+
+ ## and some maps for terrestrial ecosystems overflowing into marine areas near the coast due to aggregation and other resolution problems
+ EFG.dts %>% pivot_wider(
      id_cols = c("EFG","version","realm","biome","biome.lab","terrestrial","marine"),
      names_from = clase,
      values_from = area,
@@ -95,39 +99,29 @@ EFG.dts %>% pivot_wider(
      ) %>%
      replace_na(list(degraded=0,protected=0,unknown=0,`wild unprotected`=0)) %>%
        mutate(total=degraded+unknown+protected+`wild unprotected`) %>%
-         mutate(degraded=degraded*100/total,protected=protected*100/total,`wild unprotected`=`wild unprotected`*100/total,unknown=unknown*100/total) %>% filter(EFG %in% "F2.1") %>% select(marine,terrestrial,version,protected,degraded,`wild unprotected`,unknown,total)
+         mutate(degraded=degraded*100/total,protected=protected*100/total,`wild unprotected`=`wild unprotected`*100/total,unknown=unknown*100/total) %>% filter(EFG %in% "T2.5") %>% select(marine,terrestrial,version,protected,degraded,`wild unprotected`,unknown,total)
 
-## and some maps for terrestrial ecosystems overflowing into marine areas near the coast due to aggregation and other resolution problems
-EFG.dts %>% pivot_wider(
-    id_cols = c("EFG","version","realm","biome","biome.lab","terrestrial","marine"),
-    names_from = clase,
-    values_from = area,
-    values_fn = list(area=sum)
-    ) %>%
-    replace_na(list(degraded=0,protected=0,unknown=0,`wild unprotected`=0)) %>%
-      mutate(total=degraded+unknown+protected+`wild unprotected`) %>%
-        mutate(degraded=degraded*100/total,protected=protected*100/total,`wild unprotected`=`wild unprotected`*100/total,unknown=unknown*100/total) %>% filter(EFG %in% "T2.5") %>% select(marine,terrestrial,version,protected,degraded,`wild unprotected`,unknown,total)
-
- EFG.dts %>% group_by(clase) %>% summarise(area=sum(area))
+  EFG.dts %>% group_by(clase) %>% summarise(area=sum(area))
 
 
 
-EFG.dts %>%
-  pivot_wider(
-       id_cols = c("EFG","version","realm","biome","biome.lab","terrestrial","marine"),
-       names_from = clase,
-       values_from = area,
-       values_fn = list(area=sum)
-       ) %>%
-       replace_na(list(degraded=0,protected=0,unknown=0,`wild unprotected`=0)) %>%
-         mutate(total=degraded+unknown+protected+`wild unprotected`) %>%
-           mutate(degraded=degraded*100/total,protected=protected*100/total,`wild unprotected`=`wild unprotected`*100/total,unknown=unknown*100/total,group=ifelse(realm %in% c("T") & terrestrial & !marine,"Terrestrial",
-             ifelse(realm %in% c("F","TF") & terrestrial & !marine,"Freshwater",
-             ifelse(realm %in% c("MFT","FM",  "MT",  "TM") & terrestrial & !marine,"Transitional",
-             ifelse(realm %in% c("M", "MT", "MFT", "TM", "FM") & !terrestrial & marine,"Marine",NA))))) %>%
-             filter(!is.na(group)) ->
-              EFG.data
+ EFG.dts %>%
+   pivot_wider(
+        id_cols = c("EFG","version","realm","biome","biome.lab","terrestrial","marine"),
+        names_from = clase,
+        values_from = area,
+        values_fn = list(area=sum)
+        ) %>%
+        replace_na(list(degraded=0,protected=0,unknown=0,`wild unprotected`=0)) %>%
+          mutate(total=degraded+unknown+protected+`wild unprotected`) %>%
+            mutate(degraded=degraded*100/total,protected=protected*100/total,`wild unprotected`=`wild unprotected`*100/total,unknown=unknown*100/total,group=ifelse(realm %in% c("T") & terrestrial & !marine,"Terrestrial",
+              ifelse(realm %in% c("F","TF") & terrestrial & !marine,"Freshwater",
+              ifelse(realm %in% c("MFT","FM",  "MT",  "TM") & terrestrial & !marine,"Transitional",
+              ifelse(realm %in% c("M", "MT", "MFT", "TM", "FM") & !terrestrial & marine,"Marine",NA))))) %>%
+              filter(!is.na(group)) ->
+               EFG.data
 
+ save(file=sprintf("%s/Rdata/Degraded-protected-%s-all-versions.rda", script.dir,k), maps.x.indicators, EFG.data,d.legend, EFG.names)
+ save(file=sprintf("%s/apps/shiny/Rdata/summary.rda", script.dir), EFG.data,d.legend)
 
-save(file=sprintf("%s/Rdata/Degraded-protected-2013-all-versions.rda", script.dir), maps.x.indicators,EFG.data,d.legend, EFG.names)
-save(file=sprintf("%s/apps/shiny/Rdata/summary.rda", script.dir), EFG.data,d.legend)
+}
